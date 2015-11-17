b569ef97becc44da089a4eb4b2628d6c73cfe175
rdryan <rdryan186@yahoo.com>
2015-11-17 18:24:24 +0800

    add grap phonenumber

--- craigslist/craigslist/spiders/craigslist_spider.py
+++ craigslist/craigslist/spiders/craigslist_spider.py
@@ -3,7 +3,10 @@ import scrapy
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 from scrapy.selector import Selector
+from scrapy.http import Request
 from craigslist.items import CraigslistItem
+from urlparse import urlparse
+import re
 
 class CraigslistSpider(CrawlSpider):
     name = "craigslist_spider"
@@ -31,16 +34,39 @@ class CraigslistSpider(CrawlSpider):
             )
                     
     
+    def parseContact(self, response):
+        sel = Selector(response)
+        item = CraigslistItem()
+        content = ''.join(sel.xpath('//body/text()').extract()).replace('\n','').replace('\s+',' ')
+        item["content"] = content
+        
+        phone_num = re.findall('\d+-+\d+-+\d+',content)
+        phone_num += re.findall('\(\d+\)-+\d+-+\d+',content)
+        item["phone_num"] = phone_num
+        
+        yield item
+
+
     def parsePost(self, response):
         sel = Selector(response)
         item = CraigslistItem()
         
-        #content = sel.xpath('//section[@id="postingbody"]/a[@class="showcontact"]/@href').extract()
-        content = sel.xpath('//section[@id="postingbody"]/text()').extract()[0]
-        #item["content"] = content.replace('\n','').replace('\s+',' ')
+        content = ''.join(sel.xpath('//section[@id="postingbody"]/text()').extract()).replace('\n','').replace('\s+',' ')
+        item["content"] = content
+        
+        contact_urls = sel.xpath('//section[@id="postingbody"]/a[@class="showcontact"]/@href').extract()
+
+        if len(contact_urls) > 0:
+            contact_url = contact_urls[0]
+            url = "http://%s%s" %(urlparse(response.url).hostname, contact_url)
+            yield Request(url, callback=self.parseContact)
+
+        phone_num = re.findall('\d+-+\d+-+\d+',content)
+        phone_num += re.findall('\(\d+\)-+\d+-+\d+',content)
+        item["phone_num"] = phone_num
         
         image_url = ''.join(sel.xpath('//figure[@class="iw oneimage"]//img/@src').extract())
         item["image_url"] = image_url
         
-        return item
-
+        yield item
+    
