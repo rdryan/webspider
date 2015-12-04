2d8338e25f7e983bba2e103a3309b0f1b5e6286e
rdryan <rdryan186@gmail.com>
2015-12-04 22:24:52 +0800

    add more url

--- info/info/spiders/a114chn.py
+++ info/info/spiders/a114chn.py
@@ -7,6 +7,9 @@ from scrapy.selector import Selector
 from scrapy.http import Request
 from info.items import InfoItem
 from urlparse import urlparse
+import re
+from bs4 import BeautifulSoup
+
 
 class A114chnSpider(CrawlSpider):
     name = "a114chn"
@@ -14,6 +17,34 @@ class A114chnSpider(CrawlSpider):
     start_urls = (
         #'http://www.114chn.com/',
         'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=11&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=13&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=14&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=15&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=21&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=22&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=23&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=32&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=33&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=34&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=35&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=36&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=37&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=41&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=42&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=43&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=44&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=45&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=46&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=50&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=51&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=52&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=53&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=54&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=61&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=62&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=63&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=64&pattern=2&page=1',
+        'http://search.114chn.com/searchresult.aspx?type=1&key=%E5%B7%A5%E4%B8%9A&areaid=65&pattern=2&page=1',
     )
 
     rules = (
@@ -43,6 +74,12 @@ class A114chnSpider(CrawlSpider):
         
         yield Request(contact_url, callback=self.parse_items)
 
+    def cleanText(self,text):
+        soup = BeautifulSoup(text,'html.parser')
+        text = soup.get_text();
+        text = re.sub("( +|\n|\r|\t|\0|\x0b|\xa0|\xbb|\xab)+",' ',text).strip()
+        return text 
+
     #def parse_items(self, response):
     def parseCompany(self, response):
         #print "+++++++++", response.url
@@ -52,9 +89,9 @@ class A114chnSpider(CrawlSpider):
         #name = sel.xpath('//span[@class="company-name fl"]/text()').extract()[0]
         #contact = sel.xpath('//div[@class="bor-t"]/span/text()').extract()[0]
         name = ''.join(sel.xpath('//table[@class="tablebg"]//text()').extract())
-        print name
+        #print name
         #print contact
-        item["name"] = name.encode('utf-8')
+        item["name"] = self.cleanText(name)
         #item["contact"] = contact.encode('utf-8')
 
         #return item
