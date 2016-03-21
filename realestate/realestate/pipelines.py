dd71fa5114060b1256bfd7e844551cf290c59d20
rdryan <rdryan186@yahoo.com>
2016-03-21 15:38:03 +0800

    change output

--- realestate/realestate/pipelines.py
+++ realestate/realestate/pipelines.py
@@ -9,7 +9,7 @@ import time
 class RealestatePipeline(object):
 
     def open_spider(self, spider):
-        self.file = open('output/%s-%s.csv' % (spider.name, time.strftime("%Y-%m-%d-%H%M")), 'w+')
+        self.file = open('%s-%s.csv' % (spider.name, time.strftime("%Y-%m-%d-%H%M")), 'w+')
         self.file.write('name,price,url\n')
 
     def process_item(self, item, spider):
